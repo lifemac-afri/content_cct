59ace7e3ee0d4f9c49b094706f002595cbb36c2f
eugenedev <eugenesew4@gmail.com>
2025-02-12 16:28:33 +0000

    auth

--- src/components/headers/Sidebar.jsx
+++ src/components/headers/Sidebar.jsx
@@ -3,12 +3,13 @@ import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { FaHome, FaBlog, FaTags, FaUser, FaCog } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
+import useAuthStore from "../../store/authstore";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
-
+  const { logout } = useAuthStore();
   const menuItems = [
     { name: "Dashboard", path: "/console", icon: <FaHome /> },
     { name: "Blog Posts", path: "/posts", icon: <FaBlog /> },
@@ -47,7 +48,10 @@ const Sidebar = () => {
       </nav>
 
       <div className=" px-4 absolute bottom-5 w-full ">
-        <button className="flex items-center justify-center gap-5  w-full bg-red-400 rounded-md shadow p-2">
+        <button
+          className="flex items-center justify-center gap-5  w-full bg-red-400 rounded-md shadow p-2"
+          onClick={() => logout()}
+        >
           <BiLogOut />
           Logout
         </button>
